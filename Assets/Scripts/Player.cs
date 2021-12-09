b3802ad98876eff05375bf10d1f1d512e06a9383
valeriefonseca <95881574+valeriefonseca@users.noreply.github.com>
2021-12-09 14:29:44 -0500

    Added unity project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector3 direction;
+    public float gravity = -9.8f; // Allows me to customize how much gravity I want to apply to the player
+    public float strength = 5f; // By adjusting the strength you could possibly make the game harder
+
+    //PLAYER ANIMATION:
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
+    private void Awake() // Called when object is initialized
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f); // This will call our function AnimateSprite every 0.15 seconds 
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // You can either press spacebar or press left mouse button to raise the player
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
+
+    }
+}
